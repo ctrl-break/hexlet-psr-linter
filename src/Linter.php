73c2e2ad71ce08743ddf5e7bf64f34eb659bc7c0
fox <ctrl-break@mail.ru>
2016-08-02 00:22:17 +0400

    first test fix

--- src/Linter.php
+++ src/Linter.php
@@ -4,16 +4,19 @@ namespace HexletPsrLinter;
 
 class Linter
 {
-		public $code;
+    public $code;
 
-		public function _construct($code)
-		{
-			$this->code = $code;
-		}
+    public function __construct($code)
+    {
+        $this->code = $code;
+    }
 
     public function linter()
     {
-        if ($this->code) return true;
-				return false;
+        if ($this->code) {
+            return true;
+        }
+
+        return false;
     }
 }
--- tests/LinterTest.php
+++ tests/LinterTest.php
@@ -6,10 +6,10 @@ class LinterTest extends \PHPUnit_Framework_TestCase
 {
     public function testLinter()
     {
-        $test1 = new Linter("<?php function rightFunc() { echo \"ok\" }; ?>");
+        $test1 = new Linter('<?php function rightFunc() { echo "ok" }; ?>');
         $this->assertTrue($test1->linter());
-        
-        $test2 = new Linter("<?php function wrong_func() { echo \"-\" }; ?>");
+
+        $test2 = new Linter('<?php function wrong_func() { echo "-" }; ?>');
         $this->assertFalse($test2->linter());
     }
 }
