c0f52af410ac2529f30f65be38445023ea14c9ed
fox <ctrl-break@mail.ru>
2016-08-01 12:27:55 +0400

    travis badge

--- src/User.php
+++ src/User.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace PhpPackage;
+namespace HexletPsrLinter;
 
 class User
 {
--- tests/UserTest.php
+++ tests/UserTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace PhpPackage;
+namespace HexletPsrLinter;
 
 class UserTest extends \PHPUnit_Framework_TestCase
 {
