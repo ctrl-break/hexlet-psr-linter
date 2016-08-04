6ee455b5e60aa9ed5767d1aae991ec648c4fc9ed
fox <ctrl-break@mail.ru>
2016-08-04 18:20:18 +0400

    add fixtures

--- tests/LinterTest.php
+++ tests/LinterTest.php
@@ -2,51 +2,44 @@
 
 namespace HexletPsrLinter;
 
-class LinterTest extends \PHPUnit_Framework_TestCase
+class LinterFunctionTest extends \PHPUnit_Framework_TestCase
 {
+    private $test1;
+    private $test2;
+    private $test3;
+    private $test4;
+
+    public function setUp()
+    {
+        $this->test1 = __DIR__.DIRECTORY_SEPARATOR.'fixtures'
+                       .DIRECTORY_SEPARATOR.'function'
+                       .DIRECTORY_SEPARATOR.'test1.php';
+        $this->test2 = __DIR__.DIRECTORY_SEPARATOR.'fixtures'
+                       .DIRECTORY_SEPARATOR.'function'
+                       .DIRECTORY_SEPARATOR.'test2.php';
+        $this->test3 = __DIR__.DIRECTORY_SEPARATOR.'fixtures'
+                       .DIRECTORY_SEPARATOR.'function'
+                       .DIRECTORY_SEPARATOR.'test3.php';
+        $this->test4 = __DIR__.DIRECTORY_SEPARATOR.'fixtures'
+                       .DIRECTORY_SEPARATOR.'function'
+                       .DIRECTORY_SEPARATOR.'test4.php';
+    }
+
     public function testFuncName()
     {
-        $test1 = new Linter('
-            <?php
-                function rightFunc(){
-                }
-            ?>');
+        $test1 = new Linter(file_get_contents($this->test1));
         $result = $test1->linter();
         $this->assertEquals(0, count($result));
 
-        $test2 = new Linter('
-            <?php
-                function WrongFunc(){
-                }
-            ?>');
+        $test2 = new Linter(file_get_contents($this->test2));
         $result = $test2->linter();
         $this->assertEquals(1, count($result));
 
-        $test3 = new Linter('
-            <?php
-                function Wrong_func2(){
-                }
-            ?>');
+        $test3 = new Linter(file_get_contents($this->test3));
         $result = $test3->linter();
         $this->assertEquals(2, count($result));
 
-        $test4 = new Linter('
-            <?php
-            class ClassName extends AnotherClass
-            {
-              function __construct()
-              {
-              }
-
-              function rightFunc2()
-              {
-              }
-
-              function wrong_method3()
-              {
-              }
-            }
-            ?>');
+        $test4 = new Linter(file_get_contents($this->test4));
         $result = $test4->linter();
         $this->assertEquals(2, count($result));
     }
---
+++ tests/fixtures/function/test4.php
@@ -0,0 +1,16 @@
+<?php
+
+class ClassName extends AnotherClass
+{
+    public function __construct()
+    {
+    }
+
+    public function rightFunc2()
+    {
+    }
+
+    public function wrong_method3()
+    {
+    }
+}
