e84e1c01f92fca3af36b02b90d99fc9407a0e852
fox <ctrl-break@mail.ru>
2016-08-04 17:26:33 +0400

    add FileInspector

---
+++ src/FileInspector.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace HexletPsrLinter;
+
+class FileInspector
+{
+    private $error;
+    private $code;
+
+    public function __construct($filename)
+    {
+        if (file_exists($filename)) {
+            $file = new \SplFileInfo($filename);
+
+            if ($file->getExtension() !== 'php') {
+                $this->error = 'File must have php extension';
+            } else {
+                $this->error = false;
+                $this->code = file_get_contents($filename);
+            };
+        } else {
+            $this->error = "File doesn't exist";
+        }
+    }
+
+    public function getError()
+    {
+        return $this->error;
+    }
+
+    public function getCode()
+    {
+        return $this->code;
+    }
+}
